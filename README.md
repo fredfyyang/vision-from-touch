74853d256360bface252a4b60be9de4865bfb4d5
susan-zjc <susan-zjc@sjtu.edu.cn>
2023-09-11 10:36:25 -0400

    edit

--- README.md
+++
@@ -1,1 +0,0 @@
-# vision-from-touch
---
+++ script.js
@@ -0,0 +1,15 @@
+function changeImage() {
+    document.getElementById('out').src = './static/images/o1.png';
+}
+
+function revertImage() {
+    document.getElementById('out').src = './static/images/r1.png';
+}
+
+function changeImage_tdis() {
+    document.getElementById('tdis_out').src = './static/images/o2.png';
+}
+
+function revertImage_tdis() {
+    document.getElementById('tdis_out').src = './static/images/r2.png';
+}
